aee111bffcbd0c7a032e052a44f2839c0650a228
kerkkiatbuu <60160205@go.buu.ac.th>
2020-10-24 15:27:36 +0700

    add test GET '/'

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,17 @@
 const assert = require('assert')
+const { request } = require('http')
+const assert = require('supertest')
+const assert = require('../index')
+
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
+    })
+})
+
+describe('GET "/"', () => {
+    it('respond with Hello!', (done) => {
+        request(app).getHeader('/').expect('Hello!', done)
     })
 })
