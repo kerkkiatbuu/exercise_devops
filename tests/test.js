efbebdcaebe18fdce413c1f872a009b012b40aa9
kerkkiatbuu <60160205@go.buu.ac.th>
2020-11-11 21:38:03 +0700

    Fixed update

--- tests/test.js
+++ tests/test.js
@@ -14,13 +14,4 @@ describe('GET "/"', () => {
     })
 })
 
-describe('GET "/item"', () => {
-    it('respond with item', (done) => {
-        let want = [
-            { id: 1, name:'iPhone12 Pro Max'},
-            { id: 2, name:'Google Pixel 5'}
-        ]
-        request(app).get('/item').expect(want, done)
-    })
-})
 
